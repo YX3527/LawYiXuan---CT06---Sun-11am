df0fbc04bf136f86b3735e0e6226c9a8dd2418c5
YX3527 <lawyxyx@gmail.com>
2025-07-27 03:07:15 +0000

    Jul 27, 2025, 3:07 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -6,9 +6,9 @@ function setup() {
    background(220);//background(r, g, b)0-255eg. background(---,---,---)
 }
 function draw() {
-// ellipse(100, 100, 80, 50);       // x, y, width, height
+// ellipse(100, 100, 80, 50);                        // x, y, width, height
 // ellipse(200, 100, 80, 50);       // x, y, diametre
-// circle(300, 200, 150);           //x, y, width, height
+// circle(300, 200, 150);                            //x, y, width, height
 // circle(300, 200, 100);                                              ; is used. when dun hv can still work but when have alot
 // rect(100, 100, 100, 150, 30, 10, 15, 5) ;                               of codes, sometimes might not work
 triangle(30, 75, 58, 20, 86, 75);
