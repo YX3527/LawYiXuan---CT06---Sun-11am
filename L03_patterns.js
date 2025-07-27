c85743de941166f94d9247ab6e8ed898086a8a02
YX3527 <lawyxyx@gmail.com>
2025-07-27 04:34:35 +0000

    Jul 27, 2025, 4:34 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -4,12 +4,17 @@ function setup() {
     noLoop(); // Stops continuous drawing
   }
   
-function draw() {
-    fill(0, 0, 255)
-    circle(75, 200, 100);
-    circle(225, 200, 100);
-    circle(375, 200, 100);
-    circle(525, 200);
+// function draw() {
+//     fill(0, 0, 255)
+//     circle(75, 200, 100);
+//     circle(225, 200, 100);
+//     circle(375, 200, 100);
+//     circle(525, 200);
+
+for(let i=0; i(4); i++) {
+  circle(50 + i*5, 200, 40);
+}
+
 
   // Recap 1: Repeating Circles
 
@@ -20,4 +25,3 @@ function draw() {
   // Task 3: Row of Circles
 
   // Task 4: Grid of Circles
-}
