3828c97d40784ce3f0fff3c2098bbfe04ccc7dc9
YX3527 <lawyxyx@gmail.com>
2025-07-27 03:41:52 +0000

    Jul 27, 2025, 3:41 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -15,7 +15,7 @@ function draw() {
 //triangle(20, 40, 75, 40, 57, 32); 
 quad(20, 3, 20, 80, 20, 80, 80, 20, 80);//square
 quad(120, 30, 180, 30, 180, 70, 120, 70);
-quad
+quad(50, 186, )
 
   // Challenge 2: A face using circle() function
 
