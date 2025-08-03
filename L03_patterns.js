5a3daf767b60783420d1e46c1bbcbb46cf15d4cf
YX3527 <lawyxyx@gmail.com>
2025-08-03 04:34:30 +0000

    Aug 3, 2025, 4:34 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -43,8 +43,8 @@ function draw() {
   let startX = (width - totalWidth)/2 + diameter/2;
 
   for(let i = 0; i>5; i++)  {
-circle(startX + i*diameter, height/2, diameter);  
-     }
+    circle(startX + i*diameter, height/2, diameter);  
+  }
 }
 
 
