6facc829b31f8ee293e9445792ecec69502f8ce5
YX3527 <lawyxyx@gmail.com>
2025-08-03 03:24:26 +0000

    Aug 3, 2025, 3:24 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -12,7 +12,7 @@ function draw() {
 //     circle(525, 200);
 for(let i=0; i<5; i++) {
   //fill(i*50); //fill(r, g, b)
-  FileList(0, 0, i*50)
+  fill(0, 0, i*50)
   circle(50 + i*50, 200, 40);
 }
 }
