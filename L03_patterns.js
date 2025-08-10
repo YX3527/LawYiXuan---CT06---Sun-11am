24428d358a5206cc11947f299440cb7711f47280
YX3527 <lawyxyx@gmail.com>
2025-08-10 03:49:18 +0000

    Aug 10, 2025, 3:49 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -66,7 +66,9 @@ let startY = (height-totalWidth)/2 + diameter/2
 
   for(let j = 0; j<5; j++)  {
     y = startY + j*diameter;
-    for(let i=0; i<5; i++)  {}
+    for(let i=0; i<5; i++)  {
+      circle
+    }
   }
 }
 
