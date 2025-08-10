42c8b1d8a8fb9ad98e779e0064b900c43c29a1c0
YX3527 <lawyxyx@gmail.com>
2025-08-10 04:07:12 +0000

    Aug 10, 2025, 4:07 AM

--- L04_mouseclick.js
+++ L04_mouseclick.js
@@ -1,6 +1,19 @@
 // write your codes here
 
 
+// function setup(){
+//     createCanvas(600, 600)
+//     background(220)
+// }
+
+// function draw(){
+//     for(let i=0; i<5; i++){
+//         fill(0, 0, i*50)
+//         circle(50 + i*50, 50 + i*50, 50)
+//     }
+// }
+
+
 function setup(){
     createCanvas(600, 600)
     background(220)
