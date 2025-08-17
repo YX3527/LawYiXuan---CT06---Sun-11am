b643623b05a6db4250a4e48cd5a13d16faaaf0c4
YX3527 <lawyxyx@gmail.com>
2025-08-17 03:51:05 +0000

    Aug 17, 2025, 3:51 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -38,5 +38,5 @@ function draw(){
     let centerY = height/2;
     let topLeftX = centerX -rectSize/2
     let topLeftY = centerY -rectSize/2
-    rectResize(topLeftX, topLeftY, rectSize, )
+    rectResize(topLeftX, topLeftY, rectSize, rectSize)
 }
