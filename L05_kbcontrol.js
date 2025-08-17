857ec4e575d4537da152202a2fb7fa99792fc65c
YX3527 <lawyxyx@gmail.com>
2025-08-17 03:44:22 +0000

    Aug 17, 2025, 3:44 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -11,20 +11,20 @@
 // }
 
 
-let shapeColor ='blue';
-let circleSize =5;
-function setup(){
-    createCanvas(400,400);
-    background(220);
-    noStroke();
-}
-function mousePressed(){
-    shapeColor= color(random(255), random(255),random(255))
-    circleSize =5;
-}
-function mouseDragged(){
-    fill(shapeColor);
-    ellipse(mouseX,mouseY, circleSize,circleSize);
-    circleSize += 0.5;
-}
+// let shapeColor ='blue';
+// let circleSize =5;
+// function setup(){
+//     createCanvas(400,400);
+//     background(220);
+//     noStroke();
+// }
+// function mousePressed(){
+//     shapeColor= color(random(255), random(255),random(255))
+//     circleSize =5;
+// }
+// function mouseDragged(){
+//     fill(shapeColor);
+//     ellipse(mouseX,mouseY, circleSize,circleSize);
+//     circleSize += 0.5;
+// }
 
