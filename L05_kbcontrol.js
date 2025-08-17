92da43fa6435495e38be1a2534e92e0e04dd10f4
YX3527 <lawyxyx@gmail.com>
2025-08-17 03:26:59 +0000

    Aug 17, 2025, 3:26 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -1,12 +1,12 @@
 // write your codes here
 
-function setup(){
-    createCanvas(600, 600)
-    background(220)
-    fill(0, 200, 0)
-}
+// function setup(){
+//     createCanvas(600, 600)
+//     background(220)
+//     fill(0, 200, 0)
+// }
 
-function mouseMoved(){
-    circle(mouseX, mouseY, 50)
+// function mouseMoved(){
+//     circle(mouseX, mouseY, 50)
     //rect(mouseX, mouseY, 50, 50)
-}
+// }
