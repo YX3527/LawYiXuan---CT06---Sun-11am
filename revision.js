096bb21b945acc5b024c9a4fe676c142fd8f99e4
YX3527 <lawyxyx@gmail.com>
2025-08-24 04:04:59 +0000

    Aug 24, 2025, 4:04 AM

--- revision.js
+++ revision.js
@@ -2,11 +2,12 @@
 
 
 //square in the middle of the canvas
-function setup() {
-   createCanvas(600,600);
-   background(220);
-}
-let size = 100
-function draw(){
-   rect(width/2-size/2, height/2-size/2,size,size);
-}
+// function setup() {
+//    createCanvas(600,600);
+//    background(220);
+// }
+// let size = 100
+// function draw(){
+//    rect(width/2-size/2, height/2-size/2,size,size);
+// }
+
