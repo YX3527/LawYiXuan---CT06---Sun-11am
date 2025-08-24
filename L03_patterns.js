ddaafd1fe7b1099165bd1fc353c427fdef88afbf
YX3527 <lawyxyx@gmail.com>
2025-08-24 03:27:48 +0000

    Aug 24, 2025, 3:27 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -1,7 +1,7 @@
 //let spacing = 80; 
 // let colourValue = 0; 
 function setup() {
-    createCanvas(600, 400);                         //width then height
+    createCanvas(600, 400);                         //width then height eg. width,height)
     background(220);
     // noLoop(); // Stops continuous drawing
   }
