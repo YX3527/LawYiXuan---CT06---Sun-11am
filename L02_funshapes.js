0d2a6fa6d7a640db2896eed2ef8aa071e7acb4b4
YX3527 <lawyxyx@gmail.com>
2025-08-24 03:50:53 +0000

    Aug 24, 2025, 3:50 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -10,7 +10,7 @@ function draw() {
 // ellipse(200, 100, 80, 50);                        // x, y, diametre
 // circle(300, 200, 150);                            //x, y, width, height
 // circle(300, 200, 100);                                                          // ; is used. when dun hv can still work but when have alot
-// rect(100, 100, 100, 150, 30, 10, 15, 5) ;                                              of codes, sometimes might not work
+ rect(100, 100, 100, 150, 30, 10, 15, 5) ;                                              of codes, sometimes might not work
 //triangle(30, 75, 58, 20, 86, 75);
 //triangle(20, 40, 75, 40, 57, 32); 
 //quad(20, 3, 20, 80, 20, 80, 80, 20, 80);//square
