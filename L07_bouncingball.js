35b0313f347fafd89e78644da26887e3491647d2
YX3527 <lawyxyx@gmail.com>
2025-09-14 03:23:33 +0000

    Sep 14, 2025, 3:23 AM

--- L07_bouncingball.js
+++ L07_bouncingball.js
@@ -1,22 +1,22 @@
 // write your codes here
-let x=200;
-let y=200;
-let speed= 2;
-function setup() {
-    createCanvas(400, 400);}
-function draw(){
-     background(220);
-     //constrain within the canvas
-     x = constrain(x, 25, width-25);
-     y = constrain(y, 25, height-25)
-    if(keyIsDown(RIGHT_ARROW)){
-        x +=2 ;
-    }if(keyIsDown(LEFT_ARROW)){
-        x -=2;
-    }
-    if(keyIsDown(UP_ARROW))
-        y-=2;
-    if(keyIsDown(DOWN_ARROW))
-        y+= 2;
-    circle(x, y, 50)
-}
+// let x=200;
+// let y=200;
+// let speed= 2;
+// function setup() {
+//     createCanvas(400, 400);}
+// function draw(){
+//      background(220);
+//      //constrain within the canvas
+//      x = constrain(x, 25, width-25);
+//      y = constrain(y, 25, height-25)
+//     if(keyIsDown(RIGHT_ARROW)){
+//         x +=2 ;
+//     }if(keyIsDown(LEFT_ARROW)){
+//         x -=2;
+//     }
+//     if(keyIsDown(UP_ARROW))
+//         y-=2;
+//     if(keyIsDown(DOWN_ARROW))
+//         y+= 2;
+//     circle(x, y, 50)
+// }
