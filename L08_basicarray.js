b8d2f0796061b471439412065d47d5dfcae2aeed
YX3527 <lawyxyx@gmail.com>
2025-09-28 03:14:49 +0000

    Sep 28, 2025, 3:14 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -31,7 +31,7 @@ function setup() {
     createCanvas(400, 400);
     background(220)
 
-    fill(25, 255, 8);
+    fill(25, 259,6);
     text("My name is Yi Xuan", 50, 50);
     textSize(16);
     fill(25, 5, 5)
