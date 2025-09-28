ba73918837124a138df2d0bd125579940f8d729d
YX3527 <lawyxyx@gmail.com>
2025-09-28 04:19:42 +0000

    Sep 28, 2025, 4:19 AM

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -1,1 +1,10 @@
 // write your codes here
+
+
+ function setup() {
+     createCanvas(400, 400);
+     background(220)
+
+    fill(25, 5,5);
+    text("My name is Yi Xuan", 50, 50);
+     textSize(16);}
