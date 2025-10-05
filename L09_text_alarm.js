b25870d1ca8db43ff058349c539f043afca55b05
YX3527 <lawyxyx@gmail.com>
2025-10-05 03:29:59 +0000

    Oct 5, 2025, 3:29 AM

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -1,31 +1,49 @@
 // write your codes here
 
 // recap 
-let x = 300; 
-let speed = 2; 
+// let x = 300; 
+// let speed = 2; 
 
-function setup() {
-    createCanvas(600, 600); 
+// function setup() {
+//     createCanvas(600, 600); 
 
-    textSize(24); 
-    textAlign(LEFT); 
+//     textSize(24); 
+//     textAlign(LEFT); 
 
-}
+// }
 
-function draw() {
-    background(220); 
+// function draw() {
+//     background(220); 
 
-    text("Bounce!", x, height/2); 
+//     text("Bounce!", x, height/2); 
 
-    x +=speed; 
+//     x +=speed; 
 
     // if(x<=0 || x>= (width-4*24)) {
     //     speed *=-1; 
     // }
 
-    if(x<=0 || x>= 511) {
-        speed *=-1; 
-    }
-    console.log(x); 
+//     if(x<=0 || x>= 511) {
+//         speed *=-1; 
+//     }
+//     console.log(x); 
 
+// }
+
+
+function setup(){
+    createCanvas(600, 400)
+    textAlign(CENTER, CENTER);
+}
+function draw(){
+    background(220);
+
+    let h = hour();
+    let m = minute();
+    let s = second();
+
+    let timeString = nf(h,2) + ":" + nf(m,2) + ":" + nf(s,2);
+    textSize(72)
+    fill(0);
+    text(timeString, width / 2, height / 2);
 }
