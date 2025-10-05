abeaa38fba65515a269cd87203dff1d72711f67a
YX3527 <lawyxyx@gmail.com>
2025-10-05 04:25:21 +0000

    Oct 5, 2025, 4:25 AM

--- L10_name.js
+++ L10_name.js
@@ -1,26 +1,26 @@
 // write your codes here
 
-let interval;
-let bgColor = 220;
-function setup(){
-    createCanvas(400, 400)
-    textAlign(CENTER, CENTER);
+// let interval;
+// let bgColor = 220;
+// function setup(){
+//     createCanvas(400, 400)
+//     textAlign(CENTER, CENTER);
 
-    interval = setInterval(countdown, 1000);
+//     interval = setInterval(countdown, 1000);
 
-}
+// }
 
-function draw(){
-    background(bgColor);
+// function draw(){
+//     background(bgColor);
 
-    let s = 59-second()
-    textSize(50);
-    text(s, width/2, height/2);
+//     let s = 59-second()
+//     textSize(50);
+//     text(s, width/2, height/2);
 
 
-}
+// }
 
-function countdown(){
-    bgColor = color(random(225), random(255), random(255));
+// function countdown(){
+//     bgColor = color(random(225), random(255), random(255));
 
-}
+// }
