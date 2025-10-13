dbefc1d37158ade4f8db33d72c63a951e30cb2cb
YX3527 <lawyxyx@gmail.com>
2025-10-13 10:09:40 +0000

    Oct 13, 2025, 10:09 AM

--- Revision2.js
+++ Revision2.js
@@ -9,11 +9,10 @@ function draw() {
 
     if(x + diameter/2>width || x - diameter/2 < 0)
         speedX*=-1
-    
+}
     noStroke();
      fill(280, 0, 0);
     circle(width/2, height/2, 40);
 
     x +=speedX;
-}
 
