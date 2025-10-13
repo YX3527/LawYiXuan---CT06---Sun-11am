2d4007a43dfdc3ee1730b515355a789510b01440
YX3527 <lawyxyx@gmail.com>
2025-10-13 10:08:01 +0000

    Oct 13, 2025, 10:08 AM

--- Revision2.js
+++ Revision2.js
@@ -8,7 +8,7 @@ function draw() {
     background('lightblue')
 
     if(x + diameter/2>width || x - diameter/2 < 0)
-    if(y + width/2<diameter)
+    
     noStroke();
      fill(280, 0, 0);
     circle(width/2, height/2, 40);
