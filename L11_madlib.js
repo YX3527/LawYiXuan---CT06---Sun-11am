bd151c75bd0c86cb344d0da4574d0406be838a59
YX3527 <lawyxyx@gmail.com>
2025-10-31 10:10:57 +0000

    Oct 31, 2025, 10:10 AM

--- L11_madlib.js
+++ L11_madlib.js
@@ -34,7 +34,7 @@ function setup(){
     " Once upon a time, a (noun) (verb) (adverb) (adjective) at the (place)"
    ];
 
-   ranstory = ramdom(storyTemplpate); // choose randomly from the
+   ranstory = ramdom(storyTemplpate); // choose randomly from the array
 
 function draw(){
     background("teal");
