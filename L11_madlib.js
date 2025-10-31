4e55043953c3211bfbd7ea0e09dcc10c5c69d663
YX3527 <lawyxyx@gmail.com>
2025-10-31 10:01:00 +0000

    Oct 31, 2025, 10:01 AM

--- L11_madlib.js
+++ L11_madlib.js
@@ -29,7 +29,7 @@ function setup(){
     btnGenStory.mousePressed(buttonStoryClicked) // calls a function
 }
 
-// define the value
+// define the values for storyTemplate
 
 function draw(){
     background("teal");
