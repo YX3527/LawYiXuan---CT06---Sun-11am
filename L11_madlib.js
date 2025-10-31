2f8421127b426c48a057123ab9cf12387d2af430
YX3527 <lawyxyx@gmail.com>
2025-10-31 10:03:47 +0000

    Oct 31, 2025, 10:03 AM

--- L11_madlib.js
+++ L11_madlib.js
@@ -30,7 +30,7 @@ function setup(){
 }
 
 // define the values for storyTemplate
-   storyTemplate = ["the(adjective) (noun) went (verb)"]
+   storyTemplate = ["the (adjective) (noun) went (verb) a"]
 
 function draw(){
     background("teal");
