bed0be3962776adf117539083f8125e38574acfc
YX3527 <lawyxyx@gmail.com>
2025-10-31 10:20:49 +0000

    Oct 31, 2025, 10:20 AM

--- L11_madlib.js
+++ L11_madlib.js
@@ -60,7 +60,7 @@ function buttonStoryClicked(){
 }
 // let's join everything back
    storyText = ranstory.replace("{noun}", inputNoun.value());
-   storyText = ranstory.replace("{verb}", inputNoun.value());
-   storyText = ranstory.replace("{noun}", inputNoun.value());
+   storyText = ranstory.replace("{verb}", inputverb.value());
+   storyText = ranstory.replace("{adjective}", inputNoun.value());
    storyText = ranstory.replace("{noun}", inputNoun.value());
    storyText = ranstory.replace("{noun}", inputNoun.value());
