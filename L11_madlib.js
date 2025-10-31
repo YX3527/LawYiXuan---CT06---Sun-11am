b6484213c994581c76bc90ef6fceda68b96a6888
YX3527 <lawyxyx@gmail.com>
2025-10-31 10:18:57 +0000

    Oct 31, 2025, 10:18 AM

--- L11_madlib.js
+++ L11_madlib.js
@@ -37,7 +37,7 @@ function setup(){
    ranstory = ramdom(storyTemplpate); // choose randomly from the array
 
    // let's join everything back
-   storyText = ranstory.replace("{noun}", inputNoun.value())
+   storyText = ranstory.replace("{noun}", inputNoun.value());
 
 function draw(){
     background("teal");
