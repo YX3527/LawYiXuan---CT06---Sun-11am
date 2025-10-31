5207c314cad363de5fc6ca2c91e4250017ffde2f
YX3527 <lawyxyx@gmail.com>
2025-10-31 10:13:30 +0000

    Oct 31, 2025, 10:13 AM

--- L11_madlib.js
+++ L11_madlib.js
@@ -30,8 +30,8 @@ function setup(){
 }
 
 // define the values for storyTemplate
-   storyTemplate = ["the {adjective} {noun} went {verb} at {place).",
-    " Once upon a time, a (noun) (verb) (adverb) (adjective) at the (place)"
+   storyTemplate = ["the {adjective} {noun} went {verb} at {place}.",
+    " Once upon a time, a {noun} {verb) (adverb) (adjective) at the (place)"
    ];
 
    ranstory = ramdom(storyTemplpate); // choose randomly from the array
