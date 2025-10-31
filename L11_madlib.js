a1325a5be0b184f076a0714fb5dc2b20df00bbe2
YX3527 <lawyxyx@gmail.com>
2025-10-31 10:06:56 +0000

    Oct 31, 2025, 10:06 AM

--- L11_madlib.js
+++ L11_madlib.js
@@ -31,7 +31,7 @@ function setup(){
 
 // define the values for storyTemplate
    storyTemplate = ["the (adjective) (noun) went (verb) at (place).",
-    " Once upon a time, a (noun"
+    " Once upon a time, a (noun) (verb)"
    ]
 
 function draw(){
