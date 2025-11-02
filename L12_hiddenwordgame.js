644950c2c1e3fe87be24b8073c97b51f835846ee
YX3527 <lawyxyx@gmail.com>
2025-11-02 04:24:26 +0000

    Nov 2, 2025, 4:24 AM

--- L12_hiddenwordgame.js
+++ L12_hiddenwordgame.js
@@ -19,7 +19,7 @@ let button;
         textBox.position(width/2-textBox.width/2, 200);
 
         button = createButton('Guess');
-        button.position(width/2+textBox.width/2)
+        button.position(width/2+textBox.width/2 + textBox.width)
     }
     function draw() {
         background(220);
