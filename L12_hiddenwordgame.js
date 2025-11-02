c1db7166ce253592bd309158bcad8e14b1d84638
YX3527 <lawyxyx@gmail.com>
2025-11-02 04:32:09 +0000

    Nov 2, 2025, 4:32 AM

--- L12_hiddenwordgame.js
+++ L12_hiddenwordgame.js
@@ -21,7 +21,8 @@ function setup() {
     button.position(width/2-textBox.width/2 + textBox.width + 30, 200);
     button.mousePressed(display); 
 
-   randWord = random(words);  
+   randWord = random(words);
+   r  
 }
 
 function draw() {
