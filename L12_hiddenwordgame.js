93bbd1f81b5991e75f4fea7c36ac29b4f2280f94
YX3527 <lawyxyx@gmail.com>
2025-11-02 04:15:38 +0000

    Nov 2, 2025, 4:15 AM

--- L12_hiddenwordgame.js
+++ L12_hiddenwordgame.js
@@ -6,3 +6,5 @@ let words = [
     "elephant", "backpack", "keyboard", "hospital", "sunlight", "raincoat", "notebook", "shoulder", 
     "football", "bathroom", "sandwich", "airplane", "umbrella", "medicine", "chocolate", "software", 
     "pineapple", "furniture", "telephone", "lighthouse"];
+
+    
