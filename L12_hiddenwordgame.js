bf5c7fef03a4ae78f1766bc7695b6b7cfd28193f
YX3527 <lawyxyx@gmail.com>
2025-11-02 04:29:27 +0000

    Nov 2, 2025, 4:29 AM

--- L12_hiddenwordgame.js
+++ L12_hiddenwordgame.js
@@ -1,39 +1,42 @@
 // write your codes here
 
-
-
 let words = [
     "elephant", "backpack", "keyboard", "hospital", "sunlight", "raincoat", "notebook", "shoulder", 
     "football", "bathroom", "sandwich", "airplane", "umbrella", "medicine", "chocolate", "software", 
     "pineapple", "furniture", "telephone", "lighthouse"];
 
-let attempts = 0;
-let ranWord = "";
-let textBox;
-let button;
-let textDis;
+let attempts = 0; 
+let randWord = ""; 
+let textBox; 
+let button; 
+let textDis; 
 
-    function setup() {
-        createCanvas(600, 400);
+function setup() {
+    createCanvas(600, 400); 
 
-        textBox = createInput();
-        textBox.position(width/2-textBox.width/2, 200);
+    textBox = createInput(); 
+    textBox.position(width/2-textBox.width/2, 200); 
 
-        button = createButton('Guess');
-        button.position(width/2+textBox.width/2 + textBox.width + 30, 200);
-        button, mousePressed(display);
+    button = createButton('Guess'); 
+    button.position(width/2-textBox.width/2 + textBox.width + 30, 200);
+    button.mousePressed(display); 
 
-        ranWord = random(words);
-    }
-    function draw() {
-        background(220);
-        textAlign(CENTER, CENTER)
-        text("Guess the Word!", width/2, 50);
-        text("Attempts: " + attempts);
-        text("Hint: " + ranWord, width/2, 150);
-        text(textDis, width/2, 250)
-    }
+   randWord = random(words);  
+}
 
-    function display() {
-        textDis = textBox.value();
-    }
+function draw() {
+
+    background(220); 
+    textAlign(CENTER, CENTER); 
+    textSize(24); 
+    text("Guess the Word!", width/2, 50); 
+    text("Attempts: " + attempts, width/2, 100); 
+    text("Hint: " + randWord, width/2, 150); 
+    text(textDis, width/2, 250); 
+
+
+}
+
+function display() {
+    textDis = textBox.value(); 
+}
